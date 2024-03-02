5547d7dc2e8fb3eb23d4b556b23ae4b9f2742a48
Rohit Vishwakarma <rohit8840@gmail.com>
2024-03-02 10:48:13 +0530

    adding message

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,28 @@
 import "./Product.css";
 
-function Product({title,price}){
+function Product({title,price,features}){
+    // console.log(features);
     // console.log(props);
     // console.log(props.title);
+ 
+//  let isDiscount= price > 30000 ? "Discount is 5% " :"";
+const list=features.map((feature)=><li>{feature}</li>);
+
+
+
+
 return (
 <>
 <div className="Product">
 <h2>{title}</h2>
-<h3>price : {price/2}</h3>
+<h3>price : {(price/2)*2}</h3>
+<p>{list}</p>
+{/* <p>{isDiscount}</p> */}
+{price > 30000 ? <p>"Discount of 5%"</p> : <a href="/">Get Discount </a> }
+
+{/* <p>{features}</p> */}
+{/* <p>{features2.a}</p> */}
+{/* <p>{features2.b}</p> */}
 
 </div>
 </>
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -2,11 +2,14 @@ import Product from "./Product";
 
 function ProductTab(){
 
+    let options=["high-tech","durable","fast"];
+    // let options2={a:"high-tech",b:"durable",c:"fast"};
+
 return (
 <>
-<Product title="phone" price={30000}/>
-<Product title="laptop" price={40000}/>
-<Product title="pen" price={10}/>
+<Product title="phone" price={30000} features={options} />
+<Product title="laptop" price={40000} features={options}/>
+<Product title="pen" price={10} features={options}/>
 
 
 </>
